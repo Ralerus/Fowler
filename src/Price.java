5a6bba9f67b4e9ef789aab74af2a6db48090dd5b
d064880 <luca.carotenuto@sap.com>
2017-05-11 10:11:34 +0200

    added price class

---
+++ src/Price.java
@@ -0,0 +1,6 @@
+
+public class Price {
+	public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+}
--- src/Rental.java
+++ src/Rental.java
@@ -17,15 +17,15 @@ class Rental {
     public double amountFor(Rental each) {
         double thisAmount = 0;
         switch (each.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
+            case Price.REGULAR:
                 thisAmount += 2;
                 if (each.getDaysRented() > 2)
                     thisAmount += (each.getDaysRented() - 2) * 1.5;
                 break;
-            case Movie.NEW_RELEASE:
+            case Price.NEW_RELEASE:
                 thisAmount += each.getDaysRented() * 3;
                 break;
-            case Movie.CHILDRENS:
+            case Price.CHILDRENS:
                 thisAmount += 1.5;
                 if (each.getDaysRented() > 3)
                     thisAmount += (each.getDaysRented() - 3) * 1.5;
@@ -36,7 +36,7 @@ class Rental {
     
     public int getFrequentRenterPoints()
 	{
-		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
+		if ((getMovie().getPriceCode() == Price.NEW_RELEASE) && getDaysRented() > 1) {
 			return 2;
 		}
 		else {
